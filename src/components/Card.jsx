3f82c4ffbb2cb79e2bec4f90c9e80d7d39db3ff5
Pablo Querales <pabloquerales20@gmail.com>
2025-01-11 14:14:09 +0100

    refactorización de código

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,11 +1,10 @@
-import React, { useState,useEffect } from "react";
+import React, { useState, useEffect } from "react";
 
 export default function Card(props) {
-  const [imgPoke, setImgPoke] = useState("")
-	const[pokeType, setPokeType] = useState([])
-	const[pokeName, setPokeName] = useState("")
+	const [imgPoke, setImgPoke] = useState("");
+	const [pokeType, setPokeType] = useState([]);
+	const [pokeName, setPokeName] = useState("");
 
-	
 	useEffect(() => {
 		const requestOptions = {
 			method: "GET",
@@ -15,22 +14,21 @@ export default function Card(props) {
 		fetch(`https://pokeapi.co/api/v2/pokemon/${props.randomNumber}`, requestOptions)
 			.then((response) => response.json())
 			.then((result) => {
-				setImgPoke(result.sprites.other.dream_world.front_default)
-				setPokeName((result.name.toUpperCase()))
-				setPokeType(result.types)
+				setImgPoke(result.sprites.other.dream_world.front_default);
+				setPokeName(result.name.toUpperCase());
+				setPokeType(result.types);
 			})
 			.catch((error) => console.error(error));
 	}, [props.randomNumber]);
+
 	return (
 		<div className="card" style={{ width: "18rem" }}>
 			<img src={imgPoke} className="card-img-top" alt="ditto" />
 			<div className="card-body d-flex justify-content-center flex-column align-items-center border-top">
 				<h2 className="card-text">{pokeName}</h2>
 				<div className="d-flex gap-5">
-					{pokeType.map((types)=>{
-						return(
-							<p>{types.type.name.toUpperCase()}</p>
-						)
+					{pokeType.map((types) => {
+						return <p>{types.type.name.toUpperCase()}</p>;
 					})}
 				</div>
 			</div>
